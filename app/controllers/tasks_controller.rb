b131a99265a1d899b82e1cd686601abacf30cadf
Vera <lizeth.veraro@outlook.com>
2020-04-28 14:54:43 -0700

    First commit new rails app and Task controlers, index and display ERB view

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,14 @@
+TASKS = [
+  { number: "Task one", text: "Mark Complete"},
+  { number: "Task two", text: "Unmark Complete"},
+  { number: "Task three", text: "Mark Complete again"}
+]
+
+
+
+class TasksController < ApplicationController
+  def index
+    @tasks = TASKS
+  end
+
+end

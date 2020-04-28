db6cf4d3cf93e9138c11a1ea8c4d0bec62ab51f5
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-04-28 15:33:09 -0700

    Completed Baseline requirements

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,10 @@
+TASKS = [
+    {name: "Bob", description: "groceries", completed_at: "5pm"},
+    {name: "Jane", description: "homework", completed_at: "11am"}
+]
+
 class TasksController < ApplicationController
+    def index
+        @tasks = TASKS
+    end
 end

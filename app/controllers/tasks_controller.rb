4c0abb330a8bebab09057c11d17dd3a666dcf091
Olga Patrakova <olgapatrakova@outlook.com>
2020-04-28 16:32:35 -0700

    hardcoded data change

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,8 @@
 TASKS = [
-  'task 1',
-  'task 2',
-  'task 3'
-]
+  { name: "Task 1", description: "Do the laundry", completed_at: "20th Mar"},
+  { name: "Task 2", description: "Watch all rails videos", completed_at: "1st Apr"},
+  { name: "Task 3", description: "Do dinner", completed_at: '2nd Feb'}
+  ]
 
 class TasksController < ApplicationController
   def index

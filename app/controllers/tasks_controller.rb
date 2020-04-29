3861cee4143351abb00757d1163fc3afeec140a2
Maryam Heshmati <mairheshmati@gmail.com>
2020-04-29 16:51:53 -0700

    create task model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,21 @@
 TASKS = [
   { name: "Finish wave 1", description: "Work on the task-list project wave 1", completed_at: "April, 28, 2020" },
   { name: "Attend the Zulily meeting", description: "Attend and take notes from the Zulily company meeting", completed_at: "April, 28, 2020" },
-  { name: "Leg workout", description: "Do 45-60 mins of physical therapy leg workout for left leg", completed_at: "April, 28, 2020"  },
+  { name: "Leg workout", description: "Do 45-60 mins of physical therapy leg workout for left leg", completed_at: "April, 28, 2020" },
 ]
 
 class TasksController < ApplicationController
   def index
     @tasks = TASKS
   end
-  def show 
+
+  def show
     id = params[:id].to_i
     @task = TASKS[id]
 
     if @task.nil?
-        head :redirect
-        return 
+      head :redirect
+      return
     end
-
   end
 end

52520d1f49ab51322a81dffeb32effd7fdec39d3
Catherina <catherine.pchelina@gmail.com>
2020-04-28 19:25:50 -0700

    Add task controller, route, index and ERB view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,12 @@
+TASKS = [
+  { name: "interview prep", description: "solving algorithms on leetcode"},
+  { name: "grocery shopping", description: "buy bread, banans and apples"},
+  { name: "practice rails", description: "read videos and code along"}
+]
+
 class TasksController < ApplicationController
+
+  def index 
+    @tasks = TASKS
+  end
 end

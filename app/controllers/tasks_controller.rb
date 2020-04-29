9d9c6e01253e6cf5361564a7223e2adabba49876
Corinna Fabre <corinna.fabre@gmail.com>
2020-04-29 13:54:05 -0700

    Task model/migration and index updates

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,13 @@
-TASKS = [
-  { title: "Hidden Figures", author: "Margot Lee Shetterly"},
-  { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
-  { title: "Kindred", author: "Octavia E. Butler"}
-]
+# TASKS = [
+#   { title: "Hidden Figures", author: "Margot Lee Shetterly"},
+#   { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
+#   { title: "Kindred", author: "Octavia E. Butler"}
+# ]
 
 class TasksController < ApplicationController
 
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 
   def show

751b0e3de7bd37b15663c0cb4dc5358209126026
SuelyBarreto <suely@barreto.us>
2020-04-28 21:50:34 -0700

    Wave 0

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,12 @@
+TASKS = [
+  { name: "Add", description: "Add the files", completed_at: nil},
+  { name: "Commit", description: "Commit the changes", completed_at: nil},
+  { name: "Push", description: "Push to GitHub", completed_at: nil},
+]
+
 class TasksController < ApplicationController
+  def index
+    @tasks = TASKS
+  end
+
 end

4828cc2e03f7815959f860ecfecc09a902a1f15f
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-04-29 11:36:41 -0700

    created a controller action for the task index page which contains an array of hard-coded tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,12 @@
+ TASKS = [
+  { name: "Task_one", description: "To do something", completed_at: "time"},
+  { name: "Task_one", description: "To do something", completed_at: "time"},
+  { name: "Task_one", description: "To do something", completed_at: "time"},
+  { name: "Task_one", description: "to do something", completed_at: "time"},
+]
+
 class TasksController < ApplicationController
+  def index
+    @tasks = TASKS
+  end
 end

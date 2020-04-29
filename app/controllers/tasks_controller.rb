bc8a75d204379f5bc8b9e381a1b401a30cec55a2
Olga Patrakova <olgapatrakova@outlook.com>
2020-04-28 17:14:50 -0700

    create Task model instances, display instances on the screen

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,11 @@
-TASKS = [
-  { name: "Task 1", description: "Do the laundry", completed_at: "20th Mar"},
-  { name: "Task 2", description: "Watch all rails videos", completed_at: "1st Apr"},
-  { name: "Task 3", description: "Do dinner", completed_at: '2nd Feb'}
-  ]
+# TASKS = [
+#   { name: "Task 1", description: "Do the laundry", completed_at: "20th Mar"},
+#   { name: "Task 2", description: "Watch all rails videos", completed_at: "1st Apr"},
+#   { name: "Task 3", description: "Do dinner", completed_at: '2nd Feb'}
+#   ]
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,5 @@
 class Task < ApplicationRecord
+  task1 = Task.create(name: 'Task 1', description: "Do the laundry", completed_at: "20th Mar")
+  task2 = Task.create(name: "Task 2", description: "Watch all rails videos", completed_at: "1st Apr")
+  task3 = Task.create(name: "Task 3", description: "Do dinner", completed_at: '2nd Feb')
 end

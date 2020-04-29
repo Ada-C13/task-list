0f3087ab96b596b6f358b9430ce7b33119dc4209
Olga Patrakova <olgapatrakova@outlook.com>
2020-04-29 14:13:11 -0700

    show each task is implemented

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,16 @@
-# TASKS = [
-#   { name: "Task 1", description: "Do the laundry", completed_at: "20th Mar"},
-#   { name: "Task 2", description: "Watch all rails videos", completed_at: "1st Apr"},
-#   { name: "Task 3", description: "Do dinner", completed_at: '2nd Feb'}
-#   ]
 
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find_by( id: params[:id])
+
+
+    if @task.nil?
+      head :not_found
+      return
+    end
   end
 end

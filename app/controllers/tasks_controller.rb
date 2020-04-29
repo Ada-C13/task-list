e75a5b6f8376581e71e4a51d32310d6d0e4c4db2
Hannah <hannahjoo24@gmail.com>
2020-04-29 16:37:33 -0700

    Updated show action in tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,16 @@
-# TASKS = ['Yoga', 'Coding', 'Review CS fun', 'interview practice', 'Dancing', 'Taking a nap', 'Running']
-
-
-# TASKS = []
-
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+  end 
+
+  def show 
+    # raise
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+
+    if @task.nil? 
+      head :not_found  # it sends back an error code 404
+      return
+    end
   end 
 end

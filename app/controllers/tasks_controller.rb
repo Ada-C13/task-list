30857fce82bd2391c0c3708960840cf785bcb664
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-04-30 10:31:34 -0700

    wave 1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,7 @@
-TASKS = %w[task1 task2 task3 task4].freeze
+# TASKS = %w[task1 task2 task3 task4].freeze
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 end
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,5 @@
 module TasksHelper
+  def completed_string(task)
+    ", completed at #{task.completed_at}" unless task.completed_at.nil?
+  end
 end

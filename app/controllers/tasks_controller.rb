ab04fcfd9a5083f283d3f8f49b553fd06547b50a
Olga Patrakova <olgapatrakova@outlook.com>
2020-04-29 17:08:51 -0700

    create new actions: create, edit, update, destroy, new. Tasks/new page is displayed with a form to submit

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,12 +5,34 @@ class TasksController < ApplicationController
   end
 
   def show
-    @task = Task.find(params[:id])
-
+    @task = Task.find_by(id: params[:id])
 
     if @task.nil?
       head :not_found
       return
     end
+  end
+
+  def new
+  end
+
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description]) #instantiate a new task
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to tasks_path # go to the index so we can see the task in the list
+      return
+    else # save failed :(
+      render :new # show the new book form view again
+      return
+    end
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show'
+  get '/tasks', to: 'tasks#index', as: 'tasks' # lists all tasks
+  get '/tasks/new', to: 'tasks#new', as: 'new_task' # gets a form for a new task
+  post '/tasks', to: 'tasks#create'
+
+  get '/tasks/:id', to: 'tasks#show', as: 'book' # shows details for 1 task
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task' # brings up the form to edit the task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end

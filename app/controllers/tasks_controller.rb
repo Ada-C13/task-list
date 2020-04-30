628efecc659be9227101b5d2dc0746c4ecdba717
RebeccaRoach <rebeccaroach12@gmail.com>
2020-04-30 16:18:41 -0700

    Added new and create actions in tasks_controller, with appropriate routes and views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,32 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+  def show
+    task_id = params[:id].to_i
+    @task = Task.find(task_id)
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  # No route matches {:action=>"show", :controller=>"tasks", :id=>nil}, possible unmatched constraints: [:id]
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completed_at: params[:task][:completed_at])
+    if @task.save
+      redirect_to tasks_path
+      return
+    else
+      render :new
+      return
+    end
+  end
+
+
   # request checkbox checked (put)
   # update model, save to db
   # redirect user to reload another page
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,11 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index', as: 'tasks'
   # tasks_path helper method to give you the right url
   # when you use link_to etc in the views for user to take action on
-  
+
+  # why 'new_task' not working in localhost3000, but new is?
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  # why as singular task below?
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  post '/tasks', to: 'tasks#create'
+  
 end

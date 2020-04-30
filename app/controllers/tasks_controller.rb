408e8f8e2b47ebf4733cc40a3d1df1be9c48e320
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-04-30 16:18:25 -0700

    created a controller action show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,13 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    id = params[:id].to_i
+    @task = Task.find(id)
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
 end

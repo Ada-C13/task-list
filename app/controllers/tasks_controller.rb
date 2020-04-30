1fe70d71edf727aa10c06c330a9eb0c3499761fe
quinqu <quinruby0@gmail.com>
2020-04-30 13:45:59 -0700

    finished wave 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,13 +7,70 @@ class TasksController < ApplicationController
     end 
 
     def show 
-      id = params[:id].to_i
-      @task = Task.all[id]
-
+      task_id = params[:id]
+      @task = Task.find_by(id: task_id)
+      
+    
       if @task.nil?
-        head :not_found
-        return
+        flash[:error] = "Could not find task with id: #{task_id}"
+        redirect_to tasks_path
+      end 
+
+    end 
+
+    def new 
+      @task = Task.new
+    end 
+
+    def create
+
+      task = Task.new(
+        name: params[:task][:name], 
+        description: params[:task][:description]
+      )
+    
+      if task.save
+        redirect_to tasks_path
+        return 
+      else 
+        redirect_to new_task_path #tasks_path(new_task_path.id)
+        return 
       end 
     end 
 
+    def edit 
+      task_id = params[:id]
+      @task = Task.find_by(id: task_id)
+
+      if @task.nil?
+        flash[:error] = "Could not find task with id: #{task_id}"
+        redirect_to edit_task_path
+        return 
+      end 
+    end 
+
+    def update 
+  
+    end 
+
+    def destroy 
+
+    end 
+ 
+
+    def mark_complete
+
+    end 
+
 end
+
+
+
+
+# def update_dollars
+#   @user = User.find(params[:user_id])
+#   new_dollars = params[:updatedFunds]
+#   @user.dollars = new_dollars.to_i
+
+#   @user.save #store to database!
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,26 @@
 Rails.application.routes.draw do
   
- 
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show'
+  #verd, path routed to controller action, give path a name (prefix) for views
   
+  root :to => 'tasks#index'
+  
+  get '/tasks', to: 'tasks#index', as: 'tasks' # list all task
+  #tasks_path ^^
+  get '/tasks/new', to: 'tasks#new', as: 'new_task' #routes to post 
+
+  post '/tasks',  to: 'tasks#create' 
+
+
+  # routes that deal with specific task
+  get '/tasks/:id', to: 'tasks#show' # shows one task
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task' #edits book
+
+  patch 'tasks/:id', to: 'tasks#update'
+
+  delete '/tasks/:id', to: 'tasks#destroy'
+
+  patch '/tasks/:id/mark_complete', to: 'tasks#mark_complete', as: 'mark_complete'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

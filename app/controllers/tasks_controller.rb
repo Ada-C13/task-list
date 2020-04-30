b1fef400804fbce723910ae33c86191548b1c40d
Leah Hughes <leahwhughes@gmail.com>
2020-04-30 16:31:11 -0700

    adds view for new task form, adds new and create methods in tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,6 +4,7 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+
   def show
     id = params[:id]
     @task = Task.find_by(id: id)
@@ -15,18 +16,35 @@ class TasksController < ApplicationController
 
   end
   
+
   def new # form to add a new task
+    @task = Task.new
   end
 
+
   def create # send new task data to database
+    
+    @task = Task.new(title: params[:task][:title], description: params[:task][:description])
+    if @task.save
+      redirect_to tasks_path
+      return
+    else
+      render :new
+      return
+    end
+
   end
+
 
   def edit # form to edit details for existing task
   end
 
+  
   def update # send updated task to database
   end
 
+  
   def destroy # get rid of a task
   end
+
 end

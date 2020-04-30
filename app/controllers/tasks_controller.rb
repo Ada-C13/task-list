071013c1d276bbf1b9bd877b91f50fa47d5906d5
Yesenia <yesentorres@gmail.com>
2020-04-30 15:26:04 -0700

    created route and view, displaying array of hard-coded tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,12 @@
 class TasksController < ApplicationController
+  
+  TASKS = [
+    { description: "Wash hair"},
+    { description: "Finish readings"},
+    { description: "Make dinner"}
+  ]
+  
+  def index
+    @tasks = TASKS
+  end
 end

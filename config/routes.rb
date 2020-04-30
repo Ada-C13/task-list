3630d674063cb043f967feba08f6984902a93cd6
Jessica Liang <jessicursliang@gmail.com>
2020-04-30 16:57:47 -0700

    add routes for new task form + submission

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
 
   root to: 'tasks#index'
   get '/tasks', to: 'tasks#index', as: 'tasks'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task' # Form for new task.
+  post '/tasks', to: 'tasks#create'
+
   get '/tasks/:id', to: 'tasks#show', as: 'task'
 end

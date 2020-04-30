eb4513977ca95191af7b7df2654c22da4559a480
thenora <thenora@gmail.com>
2020-04-29 22:59:48 -0700

    added show id and show action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,5 +10,14 @@ class TasksController < ApplicationController
   def index
     @tasks = TASKS
   end
+
+  def show
+    task_id = params[:id].to_i
+    @task = BOOKS[task_id]
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
   
 end

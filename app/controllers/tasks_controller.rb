6b0dcd97fc4f74edb086433aa953c4719fd20b26
thenora <thenora@gmail.com>
2020-04-30 20:17:58 -0700

    fixed errors to get task list displaying

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -12,7 +12,7 @@ class TasksController < ApplicationController
   end
 
   def show
-    task_id = params[:id].to_i
+    task_id = params[:id]
     @task = Task.find_by(id: task_id)
     if @task.nil?
       head :not_found

9d5c749a47c78813867dc22cecc43fc0ad444532
Hannah <hannahjoo24@gmail.com>
2020-04-30 17:50:03 -0700

    Added new and create action in tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,12 +9,51 @@ class TasksController < ApplicationController
     @task = Task.find_by(id: task_id)
 
     if @task.nil? 
-      head :not_found  # it sends back an error code 404
+      redirect_to tasks_path
       return
     end
   end 
 
 
+  # def update 
+  #   @task = Task.find_by(id: params[:id])
+
+  #   if @task.nil?
+  #     head :not_found 
+  #     return 
+  #   elsif @task.update(
+  #     name: params[:task][:name],
+  #     description: params[:task][:description]
+  #   )
+
+  #   redirect_to tasks_path 
+  #   return 
+
+  #   else  
+  #     render :edit 
+  #     return 
+  #   end 
+  # end
+
+
   def new 
+    @task = Task.new
+  end
+
+  def create 
+    # raise
+    @task = Task.new(
+      name: params[:task][:name],
+      description: params[:task][:description],
+      completed_at: params[:task][:completed_at]
+    )
+
+    if @task.save
+      redirect_to task_path(@task.id)
+      return 
+    else 
+      render :new, :bad_request 
+      return 
+    end 
   end
 end

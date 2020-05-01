5513c387106e4240c67a596807799c54de7868a6
Jocelyn Wang <wangjoc@gmail.com>
2020-05-01 01:20:48 -0700

    added support to show each task

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,13 @@ class ApplicationController < ActionController::Base
   def index
     @tasks = Task.all
   end
+
+  def show
+    task_id = params[:id].to_i
+    @task = Task.all[task_id]
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
 end

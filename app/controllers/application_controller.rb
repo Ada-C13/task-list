772609ef2a22a2318dd4d2e97ab89be278d22b3f
Jocelyn Wang <wangjoc@gmail.com>
2020-05-01 06:25:41 -0700

    added in task complete function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,8 +19,8 @@ class ApplicationController < ActionController::Base
   def create
     @task = Task.new(
       name: params[:task][:name], 
-      completed_at: params[:task][:completed_at], 
-      description: params[:task][:description]
+      completed_at: nil, 
+      description: params[:task][:description],
     ) 
 
     if @task.save #
@@ -74,4 +74,22 @@ class ApplicationController < ActionController::Base
     return
   end
 
+  def completed
+    @task = Task.find_by(id: params[:id])
+
+    if @task.nil?
+      redirect_to task_path
+      return
+    end
+
+    if @task.task_completed == false
+      @task.task_completed, @task.completed_at = true, Date.today
+      @task.save
+      redirect_to tasks_path
+    else
+      @task.task_completed, @task.completed_at = false, nil 
+      @task.save
+      redirect_to tasks_path
+    end
+  end
 end

8aef6431caae96752b02c3bf5a922a6a926b9743
Haben <haben7@gmail.com>
2020-04-30 18:52:53 -0700

    added show action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,16 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+
+  def show
+    task_id = params[:id].to_i # this special params variable lets us get info that has been passed in to the controller
+    @task = Task.find_by(id: task_id)  # in this case it is the id that has been passed in through the route
+    
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
+  
 end

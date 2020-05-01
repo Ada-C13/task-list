9cf5a84be2846cc2264b3427e379e8f6bb16bfd5
cojenco <icathyouyang@gmail.com>
2020-05-01 10:23:21 -0700

    add tasks#new tasks#create, routes, ERB views. complete wave 2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,14 +11,44 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
 
+
   def show
     task_id = params[:id]
     @task = Task.find_by(id: task_id)
 
     if @task.nil?
-      head :not_found
+      redirect_to tasks_path
       return
     end
+  end
+
+
+  def new
+    @task = Task.new
+  end
+
+
+  def create
+    @task = Task.new(
+      name: params[:task][:name],
+      description: params[:task][:description],
+      completed_at: params[:task][:completed_at])
+
+    if @task.save
+      redirect_to task_path(@task.id)        #if form successfully saves, redirect to all tasks page
+    else
+      render :new, :bad_request     #if form fails to save, back to form page with form fields filled in
+    end
+  end
+
+  
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
 end

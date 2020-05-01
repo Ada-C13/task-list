931f94f7792de687f7981f479a47f1d5ba5e15a8
Jocelyn Wang <wangjoc@gmail.com>
2020-05-01 00:57:44 -0700

    completed baseline requirements

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
+TASKS = [
+  { assignee: "Jay", task: "wash the dishes"},
+  { assignee: "Sarah", task: "wash the clothes"},
+  { assignee: "Sam", task: "walk the dog"}
+]
+
 class ApplicationController < ActionController::Base
+  def index
+    @tasks = TASKS
+  end
 end

438e9ade951a44354f232ddb4e9d2b3e67134043
Vera <lizeth.veraro@outlook.com>
2020-04-30 18:54:33 -0700

    '/tasks/new' route to new task page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/tasks', to: 'tasks#index'
-  # Adding the root to task#index, to redirect to index without using/index"
-
   root :to => 'tasks#index'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+ 
+  # Adding the root to task#index, to redirect to index without using/index"
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+
   
 end

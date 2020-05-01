cdf40578dfd3ff4bf45ea51a1ab593651fdf532d
vwhwang <vwhwang38@gmail.com>
2020-05-01 13:35:22 -0700

    added form to new html and can add new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,6 +6,7 @@
 
 
 class TasksController < ApplicationController
+
   def index
     @tasks = Task.all
   end 
@@ -22,9 +23,22 @@ class TasksController < ApplicationController
   end 
 
   def new 
+    @task = Task.new
   end 
 
   def create
+    @task = Task.new(
+      name: params[:task][:name],
+      description: params[:task][:description],
+      completed_at:params[:task][:completed_at]
+    )
+    if @task.save
+      redirect_to tasks_path
+      return 
+    else 
+      render :new
+      return 
+    end 
   end 
 
 end

e801ab97ce1b4c71ff25ff37cfe9dfa1504e8eb9
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-04-30 20:00:16 -0700

    made a task table

--- db/migrate/20200501025139_create_tasks.rb
+++ db/migrate/20200501025139_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
       t.string :name
-      t.string :author
+      t.string :description
 
       t.timestamps
     end

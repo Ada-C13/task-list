19974deeb71d5ca978b1e332e92c7c4ced2c4f3b
Jocelyn Wang <wangjoc@gmail.com>
2020-05-01 03:08:03 -0700

    added in task_completed colomn to task table and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_01_080248) do
+ActiveRecord::Schema.define(version: 2020_05_01_100509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_05_01_080248) do
     t.string "completed_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "task_completed"
   end
 
 end

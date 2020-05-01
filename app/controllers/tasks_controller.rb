80968f42edb5838e750e80ee080ee2e38cd35245
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-04-30 22:20:43 -0700

    Added show method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,4 +11,14 @@ class TasksController < ApplicationController
   def index 
     @tasks = TASKS
   end
+
+  # Create a show method to show only one particular task
+  def show
+    task_id = params[:id].to_i
+    @task = TASKS[task_id]
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end 
 end

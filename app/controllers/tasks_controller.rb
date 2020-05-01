289a982f0a93f2acb021ce28afd87fc4f52f3ea3
SuelyBarreto <suely@barreto.us>
2020-04-30 19:07:23 -0700

    Wave 1 finished

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,12 @@
-TASKS = [
-  { name: "Add", description: "Add the files", completed_at: nil},
-  { name: "Commit", description: "Commit the changes", completed_at: nil},
-  { name: "Push", description: "Push to GitHub", completed_at: nil},
-]
+# TASKS = [
+#   { name: "Add", description: "Add the files", completed_at: nil},
+#   { name: "Commit", description: "Commit the changes", completed_at: nil},
+#   { name: "Push", description: "Push to GitHub", completed_at: nil},
+# ]
 
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all.sort
   end
 
 end

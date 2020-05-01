08adfda68771219542977ffd749ec63123a7cbe2
Jessica Liang <jessicursliang@gmail.com>
2020-04-30 17:29:35 -0700

    add form that saves new tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,4 +14,21 @@ class TasksController < ApplicationController
 		end
 	end
 
+	def new
+		@task = Task.new
+	end
+
+	def create
+		@task = Task.new(
+			name: params[:task][:name], 
+			description: params[:task][:description], 
+			completed_at: params[:task][:completed_at])
+		
+		if @task.save
+			redirect_to tasks_path
+		else
+			render :new, :bad_request
+		end
+	end
+
 end

ad88df8816be8e5cd11f1ce84cc7895907c1db10
Hannah <hannahjoo24@gmail.com>
2020-04-30 19:05:03 -0700

    Added edit and new actions in tasks_controller.rb

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,39 +3,6 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end 
 
-  def show 
-    # raise
-    task_id = params[:id]
-    @task = Task.find_by(id: task_id)
-
-    if @task.nil? 
-      redirect_to tasks_path
-      return
-    end
-  end 
-
-
-  # def update 
-  #   @task = Task.find_by(id: params[:id])
-
-  #   if @task.nil?
-  #     head :not_found 
-  #     return 
-  #   elsif @task.update(
-  #     name: params[:task][:name],
-  #     description: params[:task][:description]
-  #   )
-
-  #   redirect_to tasks_path 
-  #   return 
-
-  #   else  
-  #     render :edit 
-  #     return 
-  #   end 
-  # end
-
-
   def new 
     @task = Task.new
   end
@@ -53,6 +20,51 @@ class TasksController < ApplicationController
       return 
     else 
       render :new, :bad_request 
+      return 
+    end 
+  end
+
+
+  def show 
+    # raise
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+
+    if @task.nil? 
+      redirect_to tasks_path
+      return
+    end
+  end 
+
+  def edit 
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+
+    if @task.nil?
+      redirect_to root_path
+      return 
+    end
+  end
+
+  
+  def update 
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+
+    if @task.nil?
+      redirect_to root_path
+      return 
+    elsif @task.update(
+      name: params[:task][:name],
+      description: params[:task][:description],
+      completed_at: params[:task][:completed_at]
+      )
+
+      redirect_to task_path
+      return 
+
+    else  
+      render :edit, :bad_request
       return 
     end 
   end

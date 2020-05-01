17e2f6d4e5ece3f3e9c12cbfd8159a4decccef63
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-01 07:17:38 -0700

    Added index action and route for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,17 @@
+TASKS = [
+  {
+    todo: "Finish Task List project Wave 1"
+  },
+  {
+    todo: "Buy dahlia tubers"
+  },
+  {
+    todo: "Made dinner"
+  }
+]
+
 class TasksController < ApplicationController
+  def index
+    @tasks = TASKS
+  end
 end

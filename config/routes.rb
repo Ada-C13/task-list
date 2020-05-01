c026b7e25b3b9109a3dee45aceb91d9dee425d40
Hannah <hannahjoo24@gmail.com>
2020-04-30 17:49:26 -0700

    Added root and create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # verb 'path', to: 'controller#aciton'
+  root to: "tasks#index"  # root_path
   get '/tasks', to: 'tasks#index', as: 'tasks'
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create' 
 
   get '/tasks/:id', to: 'tasks#show', as: 'task'
+
+  # get '/tasks/:id', to: 'tasks#update'
+
 end

de9d712cd7e8c03a2ffcccdb77205ca5a9423f41
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-01 07:45:07 -0700

    Generated model task and updated tasks controller and index view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,17 +1,5 @@
-TASKS = [
-  {
-    todo: "Finish Task List project Wave 1"
-  },
-  {
-    todo: "Buy dahlia tubers"
-  },
-  {
-    todo: "Make dinner"
-  }
-]
-
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 end

a44c969b8a470f74614ee245a4edd3be6974d24a
Angela Nguyen <angethuy@gmail.com>
2020-05-01 00:43:34 -0700

    replaced hardcoded tasks with data from Task table

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,5 @@
-TASKS = [
-  { description: "bake a cake", assignee: "Angela" },
-  { description: "look super cute", assignee: "Picchu" },
-  { description: "cronch on carrots", assignee: "Tater Tot" },
-  { description: "stare at birds", assignee: "Elvy" }
-]
-
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 end

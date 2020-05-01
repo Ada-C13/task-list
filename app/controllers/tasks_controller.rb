e1157094361b48d9a4b377cd9beb11abbabfcc33
Olga Patrakova <olgapatrakova@outlook.com>
2020-04-30 17:04:16 -0700

    edit and update actions working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,20 +18,42 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(name: params[:task][:name], description: params[:task][:description]) #instantiate a new task
+    @task = Task.new(
+      name: params[:task][:name], 
+      description: params[:task][:description]) #instantiate a new task
+
     if @task.save # save returns true if the database insert succeeds
-      redirect_to task_path(@task.id) # go to the index so we can see the task in the list
+      redirect_to task_path(@task.id) # go to the show so we can see the new task
       return
     else # save failed :(
-      render :new # show the new book form view again
+      render :new, :bad_request # show the new task form view again, return bad request
+      return
+    end
+  end
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+
+    if @task.nil?
+      head :not_found
       return
     end
   end
 
   def update
+    @task = Task.find_by(id: params[:id])
+  if @task.nil?
+    head :not_found
+    return
+  elsif @task.update(
+    name: params[:task][:name], 
+    description: params[:task][:description])
+    redirect_to tasks_path # go to the index so we can see the task in the list
+    return
+  else # save failed :(
+    render :edit # show the new task form view again
+    return
   end
-
-  def edit
   end
 
   def destroy

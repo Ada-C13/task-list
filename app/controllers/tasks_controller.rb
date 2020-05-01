a6e4f944a5119fba084c36fd939d328541aa5119
charlotte adams <lucky.seattle.monkey@gmail.com>
2020-04-30 17:35:14 -0700

    update hard-coded data to db

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,20 +1,15 @@
 class TasksController < ApplicationController
-  TASKS = [
-    {name: "cs_fun", description: "watch video", completed_at: "Wednesday EOD"},
-    {name: "interview prep", description: "work with peer", completed_at: "Friday EOD"},
-    {name: "task_list", description: "Baseline", completed_at: "Wednesday by 11am"},
-  ]
   # Controller Actions are always methods
   # index is a method
   def index #index means to list all
-    @tasks = TASKS
+    @tasks = Task.all
   end
 
   def show
     # special params hash
     # the id was passed into the route
     id = params[:id].to_i
-    @task =  TASKS[id]
+    @task =  Task.find(id)
 
     if @task.nil?
       head :not_found
--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_04_30_211122) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "completed_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end

4148428424351d88c864ded7d5d8345faa86e3c4
Rosalyn <syrosalynyu@gmail.com>
2020-04-30 22:41:18 -0700

    completed show action and passed the tests

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,4 +9,16 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    # @task = Task.find_by(id: params[:id])
+    # @task = Task.find(params[:id])
+    @task = Task.find_by(id: params[:id])
+    # task_id = params[:id]
+    # @task = Task.find_by(id: task_id)
+    if @task.nil?
+      redirect_to tasks_path
+      return
+    end
+  end
 end

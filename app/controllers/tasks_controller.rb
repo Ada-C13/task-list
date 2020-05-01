477b5da8777e65d117e30cb41152aef2154f4342
Alicia Combs <alicia.combs.92@gmail.com>
2020-04-30 21:19:25 -0700

    Completed Wave 1: created a Task model and implemented that within the tasks_controller and index.html.erb files.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   def index
     #array of hard-coded tasks
-    @tasks_array = ["Run Laundry", "Make Tea", "Finish a Sewing Project", "Draft a Sewing Pattern", "Weed the Yard"]
+    # @tasks_array = ["Run Laundry", "Make Tea", "Finish a Sewing Project", "Draft a Sewing Pattern", "Weed the Yard"]
+    @tasks = Task.all
   end
 end

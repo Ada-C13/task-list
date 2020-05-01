c2da8bdc087627e6891cffc6a4aa1c040406952e
Nikki Vaughan <vnikki13@gmail.com>
2020-05-01 12:38:26 -0700

    Finished wave2 requirments to show individual tasks and create a new task. All tests pass

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,28 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+    if @task.nil?
+      redirect_to tasks_path
+      return
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description])
+    if @task.save
+      redirect_to task_path(@task.id)
+      return
+    else
+      render :new
+      return
+    end
+  end
 end

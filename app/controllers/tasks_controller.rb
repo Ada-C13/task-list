2f44fff0e6fa8724f78f710fefdffcbdc2085faf
Alicia Combs <alicia.combs.92@gmail.com>
2020-04-30 21:06:06 -0700

    Added tags and logic to html.erb files and minimal css

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
     #array of hard-coded tasks
-    tasks_array = ["Run Laundry", "Make Tea", "Finish a Sewing Project", "Draft a Sewing Pattern", "Weed the Yard"]
+    @tasks_array = ["Run Laundry", "Make Tea", "Finish a Sewing Project", "Draft a Sewing Pattern", "Weed the Yard"]
   end
 end

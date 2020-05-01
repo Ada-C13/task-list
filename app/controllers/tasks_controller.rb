1cba8a4d1501a047b00796d0b06109af72aaf118
thenora <thenora@gmail.com>
2020-04-30 18:48:06 -0700

    updated index and show actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,12 +8,12 @@ TASKS = [
 class TasksController < ApplicationController
 
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 
   def show
     task_id = params[:id].to_i
-    @task = BOOKS[task_id]
+    @task = Task.find_by(id: task_id)
     if @task.nil?
       head :not_found
       return

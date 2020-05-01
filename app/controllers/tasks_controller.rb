996a3eb49813aa19104b5b59c14dc2de384277d7
Maryam Heshmati <mairheshmati@gmail.com>
2020-05-01 14:46:56 -0700

    added an edit and update actions to the Tasks controller- finished wave 3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,9 +21,45 @@ class TasksController < ApplicationController
     task = Task.new name: params[:task][:name], description: params[:task][:description], completed_at: params[:task][:completed_at]
 
     if task.save
-      redirect_to task_path(task.id) ##TODO- ask why the completed at is not being saved????
+      redirect_to task_path(task.id)
     else
       render :new, :bad_request
     end
   end
+
+  def edit
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+    
+    @task.name = @task[:name]
+    @task.description = @task[:description]
+    @task.completed_at = @task[:completed_at]
+  end
+
+  def update
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+
+    @task[:name] = params[:task][:name]
+    @task[:description] = params[:task][:description]
+    @task[:completed_at] = params[:task][:completed_at]
+
+    if @task.save
+      redirect_to task_path(@task.id)
+      return
+    else
+      render :new, :bad_request
+      return 
+    end
+  end
+
+  # def destroy
+  #   book = @book.find_by(id: params[:id])
+  #   if book.nil?
+  #     head :not_found
+  #     return
+  #   end
+  #   book.destroy
+  #   redirect_to '/tasks'
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   get '/tasks', to: 'tasks#index'
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
   get '/tasks/:id', to: 'tasks#show', as: 'task' #an alias 
-
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   post '/tasks', to: 'tasks#create', as: 'create_task'
+  patch '/tasks/:id', to: 'tasks#update', as: 'update_task'
+
+  
 end

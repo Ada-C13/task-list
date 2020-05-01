1cd76d20654ba0054e61543f8090caa764a7e923
Corinna Fabre <corinna.fabre@gmail.com>
2020-05-01 10:01:49 -0700

    Functioning edit page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,13 +19,30 @@ class TasksController < ApplicationController
   end
 
   def update
+    @task = Task.find_by(id: params[:id])
+    if @task.nil?
+      head :not_found
+      return
+    elsif @task.update(
+      name: params[:task][:name],
+      description: params[:task][:description]
+    )
+      redirect_to task_path 
+      return
+    else 
+      render :new
+      return
+    end
   end
 
   def edit
     task_id = params[:id]
     @task = Task.find_by(id: task_id)
 
-
+    if @task.nil?
+      head :not_found
+      return
+    end  
   end
 
   def destroy

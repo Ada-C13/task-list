9ab6a5b6db4ea24b1184ba7dcb574035d21efeea
SuelyBarreto <suely@barreto.us>
2020-05-01 21:02:30 -0700

    Wave 2 started

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,35 @@
-# TASKS = [
-#   { name: "Add", description: "Add the files", completed_at: nil},
-#   { name: "Commit", description: "Commit the changes", completed_at: nil},
-#   { name: "Push", description: "Push to GitHub", completed_at: nil},
-# ]
-
 class TasksController < ApplicationController
+  # shows list of tasks
   def index
     @tasks = Task.all.sort
   end
 
+  # shows individual task details
+  def show
+    @task = Task.find_by(id: params[:id])
+  end
+
+  # creates a form
+  def new
+    @task = Task.new
+  end
+  
+  # form submit button calls this
+  def create
+
+    @task = Task.new(
+      name: params[:task][:name],
+      description: params[:task][:description],
+      completed_at: params[:task][:completed_at]
+    )
+    if @task.save
+      redirect_to tasks_path
+      return
+    else
+      render :new
+      return
+    end
+
+  end
+  
 end

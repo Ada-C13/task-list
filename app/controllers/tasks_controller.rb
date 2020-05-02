41f91b9301d26cac6486760df3305d0bd9aaa1a1
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-05-02 09:54:55 -0700

    added a show action and view, added links to the list of tasks to show individual task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,14 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    id = params[:id].to_i
+    # @book = Book.find(id) #this returns a Record not fount instead of a nil for an invalid id
+    @task = Task.find_by(id: id)
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
 end

58b777c77bd5afa56d2182e186dd416839e57291
Haben <haben7@gmail.com>
2020-05-01 18:31:39 -0700

    added creat, edit,  update and delete actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,5 +18,72 @@ class TasksController < ApplicationController
       return
     end
   end
-  
+
+
+  def new
+    @task = Task.new
+  end
+
+
+  def create
+    @task = Task.new(
+      name: params[:task][:name],
+      completed_at: nil,
+      description: params[:task][:description],
+    ) #instantiate a new task
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to task_path(@task.id) # go to the index so we can see the task in the list
+      return
+    else # save failed :(
+      render :new # show the new task form view again
+      return
+    end
+  end
+
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
+
+
+  def update
+    @task = Task.find_by(id: params[:id])
+    if @task.nil?
+      head :not_found
+      return
+    elsif @task.update(task_params)
+      redirect_to task_path(@task.id) # go to the index so we can see the task in the list
+      return
+    else # save failed :(
+      render :edit # show the new task form view again
+      return
+    end
+  end
+
+  def destroy
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+
+    if @task.nil?
+      head :not_found
+      return
+    end
+
+    @task.destroy
+
+    redirect_to tasks_path
+    return
+  end
+
+  private
+
+  def task_params
+    return params.require(:task).permit(:name, :description, :completed_at)
+  end
+
 end

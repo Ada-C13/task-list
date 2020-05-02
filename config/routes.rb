90ce160b1d9ff9d9ebb13b2ac9b8e2fd98ee61e2
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-02 15:49:17 -0700

    Added tasks#new and tasks#create

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
 
   # Collection of tasks
   root to: "tasks#index"
-  get "/tasks", to: "tasks#index"
+  get "/tasks", to: "tasks#index", as: "tasks"
+  get "/tasks/new", to: "tasks#new", as: "new_task"
+  post "/tasks", to: "tasks#create"
 
   # Individual tasks
   get "/tasks/:id", to: "tasks#show", as: "task"

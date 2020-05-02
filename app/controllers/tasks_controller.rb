1cec92ac5ac8f891b185ee5434abb325a7c96946
Nataliya Pogodina <n.pogodina@gmail.com>
2020-05-02 15:28:38 -0700

    Added tasks#show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,13 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    # book_id = params[:id].to_i
+    @task = Task.find_by(id: params[:id])
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # Collection of tasks
   root to: "tasks#index"
   get "/tasks", to: "tasks#index"
+
+  # Individual tasks
+  get "/tasks/:id", to: "tasks#show", as: "task"
+
 end
+
+
+  # # Routes that have to do with the collection of books
+  # get '/books', to: 'books#index', as: 'books'
+  # get '/books/new', to: 'books#new', as: 'new_book' # form
+  # post '/books', to: 'books#create'
+
+  # # Routes that deal with a specific book
+  # get '/books/:id', to: 'books#show', as: 'book'
+  # get '/books/:id/edit', to: 'books#edit', as: 'edit_book' # form
+  # patch '/books/:id', to: 'books#update'
+  # delete '/books/:id', to: 'books#destroy'
+
+  # # patch '/books/:id/mark_read', to: 'books#mark_read', as: 'book_read'

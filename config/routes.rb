594e6555ba84404e3b8615fdb6dbe67049a37415
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-05-01 20:45:39 -0700

    Finish wave two

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/tasks' => 'tasks#index', as: "tasks"
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  post '/tasks', to: 'tasks#create'
+  get '/tasks/new' => 'tasks#new', as: 'new_task'
+  post '/tasks' => 'tasks#create'
+  root to: "tasks#index"
 
   
   get '/tasks/:id' => 'tasks#show', as: "task"
-  root to: "tasks#index"
+
 end

d173e3b02aebc5a2d93952c0ddae5771377379a1
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-02 15:09:11 -0700

    adds tasks#new action, erb view with form, and new_task_path

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,8 @@ class TasksController < ApplicationController
     @task = Task.find_by(id: params[:id].to_i)
     redirect_to root_path if @task.nil?
   end
+
+  def new
+    @task = Task.new
+  end
 end

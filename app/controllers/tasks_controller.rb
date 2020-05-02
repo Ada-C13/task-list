1350933245e81b50ba2d1bcfe6ce8e558a896767
ChelseaC13 <chelhelg@outlook.com>
2020-05-01 21:09:54 -0700

    show each task and create new task in wave 2 done. Not all test passing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,11 +6,37 @@ class TasksController < ApplicationController
 
 
   def show
-    id = params[:id].to_i
-    @task = Task.find_by(id)
+    id = params[:id]
+    @task = Task.find_by(id:id)
 
     if @task.nil?
       head :not_found
+      return
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(
+      name: params[:task][:name], 
+      description: params[:task][:description], 
+      completed_at: params[:task][:completed_at]) 
+    if @task == nil?
+      render :redirect
+      return
+    end
+
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to task_path(@task.id) # go to the index so we can see the task in the list
+      return
+    # elsif @task == false
+    #   render :redirect
+    #   return
+    else # save failed :(
+      render :new # should this be :redirect or :new
       return
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
-  # verb 'path', to: 'controller#action'
-  get '/tasks', to: 'tasks#index'
-  get '/tasks/:id', to: 'tasks#show'
+  
+#  get '/tasks', to: 'tasks#index', as: 'root'  # what is this really the correct thing to do
+
+ get '/tasks', to: 'tasks#index', as: 'tasks'
+ get '/tasks/new', to: 'tasks#new', as: 'new_task'
+ post '/tasks', to: 'tasks#create'
+
+ # Routes that operate on individual books
+ get '/tasks/:id', to: 'tasks#show', as: 'task'
+ get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+ patch '/tasks/:id', to: 'tasks#update'
+ delete '/tasks/:id', to: 'tasks#destroy'
+
+  
+
 end

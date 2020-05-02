6c36f220dd5103cfac107b2e742854c6812092d1
Catherina <catherine.pchelina@gmail.com>
2020-05-02 13:56:00 -0700

    Add new and create routes, add new and create actions, add new.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
 
   get '/tasks', to: 'tasks#index', as: 'tasks'
 
+  get '/tasks/new', to: 'tasks#new', as: 'new_task' # order matters!
+  # if tasks/new goes after tasks/:id, rails will use tasks/:id route!
+
+  post '/tasks', to: 'tasks#create'
+
   get '/tasks/:id', to: 'tasks#show', as: 'task'
+  
 end

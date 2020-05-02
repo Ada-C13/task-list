18396d67c5312e3dd1c2ff2a3217773f64e084a2
cojenco <icathyouyang@gmail.com>
2020-05-01 17:04:49 -0700

    add tasks#edit tasks#update, routes, ERB views. wave 3 required done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -35,20 +35,53 @@ class TasksController < ApplicationController
       completed_at: params[:task][:completed_at])
 
     if @task.save
-      redirect_to task_path(@task.id)        #if form successfully saves, redirect to all tasks page
+      redirect_to task_path(@task.id)    #if form successfully saves, redirect to all tasks page
     else
-      render :new, :bad_request     #if form fails to save, back to form page with form fields filled in
+      render :new, :bad_request          #if form fails to save, back to form page with form fields filled in
     end
   end
 
   
   def edit
+    @task = Task.find_by(id: params[:id])
+
+    if @task.nil?
+      head :not_found
+      return
+    end
   end
 
+
   def update
+    @task = Task.find_by(id: params[:id])
+    return head :not_found if @task.nil?
+
+    if @task.update(                     #if form successfully updates/saves 
+      name: params[:task][:name],
+      description: params[:task][:description],
+      completed_at: params[:task][:completed_at]
+    )
+      redirect_to task_path(@task.id)    #redirect to all tasks page
+    else
+      render :edit, :bad_request         #if form fails to save, back to form page with form fields filled in
+    end
   end
 
   def destroy
   end
 
 end
+
+
+  # def update
+  #   @task = Task.find_by(id: params[:id])
+  #   @task.name = params[:task][:name]
+  #   @task.description = params[:task][:description]
+  #   @task.completed_at = params[:task][:completed_at]
+
+  #   if @task.save
+  #     redirect_to task_path(@task.id)    #if form successfully saves, redirect to all tasks page
+  #   else
+  #     render :edit, :bad_request          #if form fails to save, back to form page with form fields filled in
+  #   end
+  # end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # Verb 'path', to: 'controller#action'
+  # Verb "path", to: "controller#action", as: "prefix"    # prefix is a nickname for the path, and can help maintain naming
   # Add routes that deal with the entire collection of tasks
   get "/tasks", to: "tasks#index", as: "tasks"            # list all tasks
   get "/tasks/new", to: "tasks#new", as: "new_task"       # get form for new task

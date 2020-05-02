3d60f6822e0a2c94b90edc3dfe614a61d916256a
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-02 15:41:28 -0700

    adds tasks#create action and post route, redirects to #show if successful

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,9 +6,16 @@ class TasksController < ApplicationController
   def show
     @task = Task.find_by(id: params[:id].to_i)
     redirect_to root_path if @task.nil?
+    return
   end
 
   def new
     @task = Task.new
+  end
+
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description])
+    @task.save ? (redirect_to action: "show", id: @task[:id]) : (render :new)
+    return
   end
 end

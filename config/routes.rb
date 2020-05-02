9e72b21a6fb29037051c73e6edd352dae4a03d62
Jetmire Bajrami <jetmirebajrami@Jetmires-MacBook-Pro.local>
2020-05-01 18:35:42 -0700

    updated the index viwe, add the link to the add new task

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/tasks' => 'tasks#index', as: "tasks"
-  get '/tasks/new', to: 'tasks#new', as: 'new_book'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
   post '/tasks', to: 'tasks#create'
 
   

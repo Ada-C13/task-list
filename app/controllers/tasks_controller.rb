4f2c5e775de242a9a6bca2e0029f7c385f845183
Yieni <yaynee8d@gmail.com>
2020-05-02 16:07:07 -0700

    create controller, routes, and view with dummy data

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,10 @@
+TASKS = [
+  {name: 'Laundry'}, 
+  {name: 'Fold laundry'}, 
+  {name: 'Clean kitchen'} 
+]
 class TasksController < ApplicationController
   def index
+    @tasks = TASKS
   end
 end

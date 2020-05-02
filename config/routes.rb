c3c023cedd123f602da97db5e26c98d6fd81e9da
Yolotzin Dunbar <yltzndnbr@gmail.com>
2020-05-01 19:30:53 -0700

    Added new and create actions with routes, ERB form and link.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # Routes that operate on collection
   get '/tasks', to: 'tasks#index', as: 'tasks'
-  
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
+
   # Routes that operate on individual tasks
   get '/tasks/:id', to: 'tasks#show', as: 'task'
 end

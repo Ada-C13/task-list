0bfea51f8625086517f827029574ad7e107c2e6f
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-02 16:04:16 -0700

    adds alert messages if #show and #create are unsuccessful

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,7 @@ class TasksController < ApplicationController
 
   def show
     @task = Task.find_by(id: params[:id].to_i)
-    redirect_to root_path if @task.nil?
+    (redirect_to root_path, alert: "Error: Task not found") if @task.nil?
     return
   end
 
@@ -15,7 +15,7 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.new(name: params[:task][:name], description: params[:task][:description])
-    @task.save ? (redirect_to task_path(@task)) : (render :new)
-    return
+    @task.save ? (redirect_to task_path(@task)) : (render :new, alert: "Error: Task not saved")
+    return 
   end
 end

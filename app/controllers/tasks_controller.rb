59b43edde6bcab6345368be5a3b81271ec2f99a3
ChelseaC13 <chelhelg@outlook.com>
2020-05-01 22:33:42 -0700

    edit page creaated still need to pre fill input boxes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -40,4 +40,28 @@ class TasksController < ApplicationController
       return
     end
   end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+    # name = params[:task][:name]   
+    # @task.description = params[:task][:description]
+    # @task.completed_at = params[:task][:completed_at]
+
+    if @task.nil?
+      head :not_found
+      return
+    elsif @task.update(
+      name: params[:task][:name], 
+      description: params[:task][:description], 
+      completed_at: params[:task][:completed_at]
+    )
+      redirect_to task_path # go to the task show page not in the list
+      return
+    else # save failed where did we save? did update do it?
+      render :edit # show the edit task form view again
+      return
+    end
+  end
+
+
 end

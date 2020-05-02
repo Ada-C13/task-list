444f90e1b7073fb9b57c3ddbb9327423563b6e62
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-01 23:45:13 -0700

    Updated the methods to the tasks_controller to use the model rather than the hard-coded data

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,21 +1,21 @@
-TASKS = [
-  {name:"Task One", description: "Complete reading lessons"},
-  {name:"Task Two", description: "Complete Exercises"},
-  {name:"Task Three", description: "Work on the project"},
-  {name:"Task Four", description: "Spend at least an hour working on CS Fundamental"}
-]
+# TASKS = [
+#   {name:"Task One", description: "Complete reading lessons"},
+#   {name:"Task Two", description: "Complete Exercises"},
+#   {name:"Task Three", description: "Work on the project"},
+#   {name:"Task Four", description: "Spend at least an hour working on CS Fundamental"}
+# ]
 
 
 class TasksController < ApplicationController
   # Create an index method to show the list of all tasks
   def index 
-    @tasks = TASKS
+    @tasks = Task.all
   end
 
   # Create a show method to show only one particular task
   def show
     task_id = params[:id].to_i
-    @task = TASKS[task_id]
+    @task = Task.find(task_id)
     if @task.nil?
       head :not_found
       return

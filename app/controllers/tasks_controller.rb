237e5a3f53faf5f2ade52b7e7323e1dc1e0fad9e
Antonia <antonianomamiukor@gmail.com>
2020-05-03 01:49:01 -0700

    updated controller's index and corresponding view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,10 @@
-TASKS = [
-    { task: "Task 1"},
-    { task: "Task 2"},
-    { task: "Task 3"}
-  ]
+# TASKS = [
+#     { task: "Task 1"},
+#     { task: "Task 2"},
+#     { task: "Task 3"}
+#   ]
 class TasksController < ApplicationController
   def index
-    @tasks = TASKS
+    @tasks = Task.all
   end
 end

09bde1e8325c832798ef52d13d2e96aa15dcccd2
Faezeh Ashtiani <faezeh.ashtiani@gmail.com>
2020-05-03 12:55:00 -0700

    passes vave 2 test

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,11 +7,59 @@ class TasksController < ApplicationController
 
   def show
     id = params[:id].to_i
-    # @book = Book.find(id) #this returns a Record not fount instead of a nil for an invalid id
+    # @task = Task.find(id) #this returns a Record not fount instead of a nil for an invalid id
     @task = Task.find_by(id: id)
+    if @task.nil?
+      redirect_to tasks_path
+      return
+    end
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    # raise
+    # "task"=>{"name"=>"boo", "description"=>"bi", "completed_at"=>"boo"},
+    @task = Task.new(
+      name: params[:task][:name],
+      description: params[:task][:description],
+      completed_at: params[:task][:completed_at]
+    )
+
+    if @task.save
+      redirect_to task_path(@task.id)
+    else
+      render :new, :bad_request # show the new task form view again
+    end
+  end
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+
     if @task.nil?
       head :not_found
       return
     end
   end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+    if @task.nil?
+      head :not_found
+      return
+    elsif @task.update(
+      name: params[:task][:name],
+      description: params[:task][:description],
+      completed_at: params[:task][:completed_at]
+    )
+      redirect_to task_path(@task.id) 
+      return
+    else # save failed :(
+      render :edit
+      return
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   get '/tasks', to: 'tasks#index', as: 'tasks'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task' # gets a form for a new task
+  post '/tasks', to: 'tasks#create' #  submits the form to post a new task
 
   get '/tasks/:id', to: 'tasks#show', as: 'task'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task' # Brings up the form to edit a task
+  patch '/tasks/:id', to: 'tasks#update'
+  delete '/tasks/:id', to: 'tasks#destroy'
 
 end

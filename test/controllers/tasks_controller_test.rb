2e5817342d95e61013a3198bf3a13c8388c268e9
Yieni <yaynee8d@gmail.com>
2020-05-02 22:27:16 -0700

    refactor wave 2

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -79,40 +79,38 @@ describe TasksController do
     end
   end
   
-  # Unskip and complete these tests for Wave 3
-  # describe "edit" do
-  #   it "can get the edit page for an existing task" do
-  #     skip
-  #     # Your code here
-  #   end
+ #Unskip and complete these tests for Wave 3
+  describe "edit" do
+    it "can get the edit page for an existing task" do
+      # Your code here
+    end
     
-  #   it "will respond with redirect when attempting to edit a nonexistant task" do
-  #     skip
-  #     # Your code here
-  #   end
-  # end
+    it "will respond with redirect when attempting to edit a nonexistant task" do
+      # Your code here
+    end
+  end
   
-  # # Uncomment and complete these tests for Wave 3
-  # describe "update" do
-  #   # Note:  If there was a way to fail to save the changes to a task, that would be a great
-  #   #        thing to test.
-  #   it "can update an existing task" do
-  #     # Your code here
-  #   end
+  # Uncomment and complete these tests for Wave 3
+  describe "update" do
+    # Note:  If there was a way to fail to save the changes to a task, that would be a great
+    #        thing to test.
+    it "can update an existing task" do
+      # Your code here
+    end
     
-  #   it "will redirect to the root page if given an invalid id" do
-  #     # Your code here
-  #   end
-  # end
+    it "will redirect to the root page if given an invalid id" do
+      # Your code here
+    end
+  end
   
-  # # Complete these tests for Wave 4
-  # describe "destroy" do
-  #   # Your tests go here
+  # Complete these tests for Wave 4
+  describe "destroy" do
+    # Your tests go here
     
-  # end
+  end
   
-  # # Complete for Wave 4
-  # describe "toggle_complete" do
-  #   # Your tests go here
-  # end
+  # Complete for Wave 4
+  describe "toggle_complete" do
+    # Your tests go here
+  end
 end

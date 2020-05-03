b07bd4e4a5c15932aef4ecfd037af52ee35ca4e4
Yieni <yaynee8d@gmail.com>
2020-05-02 19:02:47 -0700

    fix show route and add controller action and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   get '/tasks/new', to:'tasks#new', as: 'new_tasks'
   post '/tasks', to: 'tasks#create'
 
-  get '/task/:id', to: 'tasks#show', as: 'task'
-  get '/task/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   patch '/tasks/:id', to: 'task#update'
   delete '/tasks/:id', to: 'tasks#destroy'
 end

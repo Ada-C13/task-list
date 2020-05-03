63e7fb2da69caf1938889995c5c23e3ac2a12767
Antonia <antonianomamiukor@gmail.com>
2020-05-03 15:13:41 -0700

    modified view and fixed error in tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,14 @@
-# TASKS = [
-#     { task: "Task 1"},
-#     { task: "Task 2"},
-#     { task: "Task 3"}
-#   ]
 class TasksController < ApplicationController
   def index
     @tasks = Task.all
+  end
+  
+  def show
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+    if @task.nil?
+      head :not_found
+      return
+    end
   end
 end

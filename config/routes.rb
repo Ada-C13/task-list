d5fce22ccbc4fe9d308b635973db7260ee410a77
SuelyBarreto <suely@barreto.us>
2020-05-04 09:13:50 -0700

    giving css credit

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
-# verb 'path', to: 'controller#action'
+# verb 'path',      to: 'controller#action'
 Rails.application.routes.draw do
+  # routes from most to least specific
+  root              to: 'tasks#index'
+  get '/tasks',     to: 'tasks#index', as: 'tasks'
+  get '/tasks/new', to: 'tasks#new',   as: 'new_task'
+  post '/tasks',    to: 'tasks#create'
 
-  root to: 'tasks#index'
-  get '/tasks', to: 'tasks#index', as: 'tasks'
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  post '/tasks', to: 'tasks#create'
-
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  get '/tasks/:id',      to: 'tasks#show', as: 'task'
   get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  patch '/tasks/:id', to: 'tasks#update'
-  patch '/tasks/:id/complete', to: 'tasks#complete', as: 'complete_task'
+  patch '/tasks/:id',    to: 'tasks#update'
+  patch '/tasks/:id/complete',   to: 'tasks#complete',   as: 'complete_task'
   patch '/tasks/:id/uncomplete', to: 'tasks#uncomplete', as: 'uncomplete_task'
-  delete '/tasks/:id', to: 'tasks#destroy'
+  delete '/tasks/:id',           to: 'tasks#destroy'
 
 end

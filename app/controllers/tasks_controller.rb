d8f204275e162eb7788352f72766652da4f83e64
Yesenia <yesentorres@gmail.com>
2020-05-04 14:19:10 -0700

    added create function

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,7 +15,19 @@ class TasksController < ApplicationController
 
   def new
     @task = Task.new
+    render :new
   end 
+
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description]) #instantiate a new task
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to tasks_path # go to the index so we can see the book in the list
+      return
+    else # save failed :(
+      render :new # show the new book form view again
+      return
+    end
+  end
 
   def update
   end
@@ -28,7 +40,6 @@ class TasksController < ApplicationController
 
 
 
-  def create
-  end 
+
 
 end

88a748c147abc2881745a1dc0ba9e3477e61b70e
Jessica Liang <jessicursliang@gmail.com>
2020-05-04 16:27:01 -0700

    resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,5 @@ Rails.application.routes.draw do
 
   resources :tasks
 
-  # get '/tasks', to: 'tasks#index', as: 'tasks'
-  # get '/tasks/new', to: 'tasks#new', as: 'new_task' # Form for new task.
-  # post '/tasks', to: 'tasks#create'
-
-  # get '/tasks/:id', to: 'tasks#show', as: 'task'
-  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task' # Form for editing existing task.
-  # patch '/tasks/:id', to: 'tasks#update'
   patch '/tasks/:id/complete', to: 'tasks#completed?', as: 'complete_task' # Mark completion of task.
-  # delete '/tasks/:id', to: 'tasks#destroy'
 end

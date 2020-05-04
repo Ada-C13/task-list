5c3c7e01f1132ec27ede026fe3ac848e2bf3d6f9
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-05-03 18:25:56 -0700

    fixed snytax in delete route

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   get "/tasks/:id", to: "tasks#show", as: "task"
   get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
   patch "/tasks/:id", to: "tasks#update"
-  delete "/tasks/;id", to: "tasks#destory"
+  delete "/tasks/:id", to: "tasks#destory"
+
+  # patch "/tasks/:id/mark_read" to: "tasks#read", as:'task_read'
 end

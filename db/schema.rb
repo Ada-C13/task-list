dd09299bb02548fa37b93110bc2979b75fca8d51
kate myer <kate.d.mangubat@gmail.com>
2020-05-04 12:57:20 -0700

    updated db

--- db/schema.rb
+++ db/schema.rb
@@ -11,5 +11,16 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 0) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
+  create_table "tasks", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "duedate"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "completed_at"
+  end
+  
 end

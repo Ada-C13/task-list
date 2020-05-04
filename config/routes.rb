d40ce14cf71f6ed5b8f526e9b28c7a8eadfca2f8
Vera <lizeth.veraro@outlook.com>
2020-05-03 17:29:43 -0700

    New routes, edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/tasks', to: 'tasks#index'
+  # Adding the root to task#index, to redirect to index without using/index"
   root :to => 'tasks#index'
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
- 
-  # Adding the root to task#index, to redirect to index without using/index"
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  post "tasks", to: "tasks#create"
 
 
   # *ROUTS TO DEAL WITH SPECIFIC BOOK.
   # To post the to the DB.
-  post 'tasks', to: 'tasks#create'
+ 
+  get "/tasks/:id", to: "tasks#show", as: "task"
+  get "/tasks/:id/edit", to: "tasks#edit", as: "edit_task" # Brings up  the form to edit a task.
+  patch "/tasks/:id", to: "tasks#update" # To update and existing task.
+
 
 
   

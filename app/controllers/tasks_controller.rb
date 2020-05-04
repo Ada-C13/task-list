2b9c515ba779cca0fce4c68539391d5a34361d27
Haben <haben7@gmail.com>
2020-05-04 11:39:52 -0700

    completed wave 4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,7 +8,6 @@ class TasksController < ApplicationController
     @task = Task.find_by(id: task_id)  
     
     if @task.nil?
-      # head :not_found
       redirect_to tasks_path
       return
     end

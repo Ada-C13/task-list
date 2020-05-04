7eaaaa9cf73a48e61df8b750dd00e6c27af7bc28
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-05-04 00:17:25 -0700

    added a patch route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   #Add Routes that have to with the collection of tasks
+  root to: "tasks#index"
+  # resources: :tasks
   get "tasks", to: "tasks#index", as: "tasks" #all books
   get "tasks/new", to: "tasks#new", as: "new_task" #gets a form for new
   post "/tasks", to: "tasks#create"
@@ -11,6 +13,6 @@ Rails.application.routes.draw do
   get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
   patch "/tasks/:id", to: "tasks#update"
   delete "/tasks/:id", to: "tasks#destory"
-
-  # patch "/tasks/:id/mark_read" to: "tasks#read", as:'task_read'
+  patch "/tasks/:id/mark_complete", to: "tasks#mark_complete", as: "complete"
+  patch "/tasks/:id/unmark_complete", to: "tasks#unmark_complete", as: "unmark"
 end

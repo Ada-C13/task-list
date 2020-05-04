865f1f0da5588b9ade484bb2dd15957ad869935a
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-05-03 18:23:32 -0700

    refactor on show method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,26 +4,70 @@ class TasksController < ApplicationController
   end
 
   def show
-    task_id = params[:id].to_i
-    @task = Task.find_by(id: task_id)
+    @task = Task.find_by(id: params[:id].to_i)
+    if @task.nil?
+      head :not_found
+      redirect_to tasks_path
+      return
+    end
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+    if @task.nil?
+      head :not_found
+      return
+    elsif @task.update(
+      name: params[:task][:name],
+      description: params[:task][description],
+    )
+      redirect_to task_path
+      return
+    else
+      render :edit
+      return
+    end
+  end
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+
     if @task.nil?
       head :not_found
       return
     end
   end
 
-  def update
-  end
-
-  def edit
-  end
-
   def destory
+    task_id = params[:id]
+    @task = Task.find_by(id: task_id)
+    if @task.nil?
+      head :not_found
+      return
+    else
+      @task.destroy
+      redirect_to tasks_path
+    end
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    task = Task.new(
+      name: params[:task][:name],
+      description: params[:task][:description],
+    )
+    if task.save
+      redirect_to tasks_path
+      return
+    else
+      render :new, :bad_request
+      return
+    end
+  end
+
+  def read
   end
 end

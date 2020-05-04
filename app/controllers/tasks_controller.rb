dcdd0a9943dfc02ecb5a6b107d95eafdc33cac75
Halahaddad1 <h.m.haddad@gmail.com>
2020-05-04 07:36:28 -0700

    created db and added 3 tasks to it, also updated index and view

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,21 @@
-TASKS = [
-    {task: "Clean the dishes"},
-    {task: "Take out the trash"},
-    {task: "Take the kids to the park"}
-]
+# TASKS = [
+#     {task: "Clean the dishes"},
+#     {task: "Take out the trash"},
+#     {task: "Take the kids to the park"}
+# ]
 
 
 class TasksController < ApplicationController
     def index
-        @tasks= TASKS
+        @tasks= Task.all
+    end
+
+    def show
+        task_id= params[:id]
+        @tasks = Task.find_by(id: task_id)
+        if @task.nil?
+            head :not_found
+            return
+        end
     end
 end

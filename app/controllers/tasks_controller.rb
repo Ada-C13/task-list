cdfcdce59ac9ebba14e83271445015db35bd3ea3
kate myer <kate.d.mangubat@gmail.com>
2020-05-04 13:14:48 -0700

    created task list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,11 @@
-TASKS = []
+TASKS = [
+  { title: "Kids", description: "Get more workbooks"},
+  { title: "Home", description: "Get more toothpaste"},
+  { title: "Home", description: "Get coffee now"},
+  { title: "Charlie", description: "Update shots"},
+  { title: "Costco", description: "Toilet Paper"},
+  { title: "Costco", description: "Chicken"},
+]
 
 class TasksController < ApplicationController
   

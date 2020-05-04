420b9a978c5479e14909fe9ee8ce2ff068d7a447
Corinna Fabre <corinna.fabre@gmail.com>
2020-05-04 11:46:30 -0700

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root :to => 'tasks#index'
-  get '/tasks', to: 'tasks#index', as: 'tasks'
+  root to: 'tasks#index'
+  resources :tasks
 
-  ##PAIR -- get, post -- to add new
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  post '/tasks', to: 'tasks#create'
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
-  ##PAIR -- get, patch -- to update existing
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
-  patch '/tasks/:id', to: 'tasks#update', as: 'update' 
+  #Leaving because of delete_path
   delete '/tasks/:id', to: 'tasks#destroy', as: 'delete'
 
+  #Leaving because of completed_path
   get '/tasks/:id/mark_complete', to: 'tasks#mark_complete', as: 'completed'
   
 end

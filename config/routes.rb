cc285c94ab2c0dc0d92bc4b9258c80bdebf992d6
Tithvorlak Mok <tithvorlakmok@gmail.com>
2020-05-04 23:57:58 -0700

    wrote some tests but not all

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  root to: 'tasks#index' # set the index page to be the homepage instead of "Welcome to Rails" splash page
+  
   # Routes that operate on the list of tasks
   get '/tasks', to: 'tasks#index', as: 'tasks' # list of all tasks
   get '/tasks/new', to: 'tasks#new', as: 'new_task' # gets a form for a new task

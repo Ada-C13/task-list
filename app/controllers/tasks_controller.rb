a59dd29cba5b55808a3495b5c3aa4342bfb23c82
Angela Nguyen <angethuy@gmail.com>
2020-05-05 00:36:04 -0700

    fixed bug where task list was out of order whenever tasks were edited

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = Task.order("id")
   end
 
   def show
@@ -13,7 +13,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completed_at: params[:task][:completed_at])
+    @task = Task.new(task_params)
 
     if @task.save 
       return redirect_to @task
@@ -26,6 +26,18 @@ class TasksController < ApplicationController
     @task = Task.find_by(id: params[:id])
     return redirect_to :tasks if @task.nil?
   end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+    return redirect_to :tasks if @task.nil?
+    return redirect_to task_path if @task.update(task_params)
+    return render :edit
+  end
+
+  def task_params
+    return params.require(:task).permit(:name, :description, :completed_at)
+  end
+
 
 
 end

b9fa19af283a2edc3f19fa7fa8c733a79f2fe753
Haben <haben7@gmail.com>
2020-05-04 21:17:01 -0700

    added resources instead of 7 routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  
-  root to: 'tasks#index'
-  get '/tasks', to: 'tasks#index', as: 'tasks' 
-  get '/tasks/new', to: 'tasks#new', as: 'new_task' 
-  post '/tasks', to: 'tasks#create' 
+  root to: 'tasks#index' 
 
-  get '/tasks/:id', to: 'tasks#show', as: 'task' 
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task' 
-  patch '/tasks/:id', to: 'tasks#update' 
-  delete '/tasks/:id', to: 'tasks#destroy' 
+  resources :tasks
   patch '/tasks/:id/completed_at', to: 'tasks#completed_at', as: 'completed_task'
   
 end

2601747f60dcb21d8d203ddd3cba209361b6cd71
mulhoo <oliviamulholland94@gmail.com>
2020-05-04 18:33:16 -0700

    Wave 3, tests passing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,4 +24,30 @@ class TasksController < ApplicationController
     redirect_to task_path(@new_task.id) if @new_task.save
 
   end
+
+  def edit
+    @task = Task.find_by(id: params[:id])
+    redirect_to tasks_path if @task.nil?
+  end
+
+  def update
+    task = Task.find_by(id: params[:id])
+
+    if task.nil?
+      redirect_to root_path
+    elsif task.update(task_params)
+      redirect_to task_path(task.id)
+    else
+      render new_task_path
+    end
+  end
+
+
+
+  private
+
+  def task_params
+    return params.require(:task).permit(:name, :description, :completed)
+  end
+  
 end

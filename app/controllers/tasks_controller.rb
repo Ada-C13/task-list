663edaf1a0371c993ed679fd792b7c4ee1235297
Halahaddad1 <h.m.haddad@gmail.com>
2020-05-04 18:41:09 -0700

    created show/create/edit/destroy in tasks controller and added to routes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,18 +4,89 @@
 #     {task: "Take the kids to the park"}
 # ]
 
-
 class TasksController < ApplicationController
     def index
         @tasks= Task.all
     end
+    
+    def new
+        @task = Task.new
+    end
 
+    def create
+        @task = Task.new(name: params[:task][:name],description: params[:task][:description],completed_at: params[:task][:completed_at]) 
+        if @task.save
+          redirect_to task_path(@task.id)
+          return
+        else 
+          render :new 
+          return
+        end
+    end
+    
     def show
         task_id= params[:id]
-        @tasks = Task.find_by(id: task_id)
+        @task = Task.find_by(id: task_id)
+        if @task.nil?
+            redirect_to tasks_path
+            return
+        end
+    end
+
+    def edit
+        @task = Task.find_by(id: params[:id])
         if @task.nil?
             head :not_found
             return
         end
     end
+
+    def update
+        @task= Task.find_by(id: params[:id])
+        if @task.nil?
+            head :not_found
+            return 
+        elsif @task.update(
+            name: params[:task][:name],
+            description: params[:task][:description],
+            completed_at: params[:task][:completed_at]
+        )
+            redirect_to tasks_path
+            return
+        else
+            render :edit
+            return
+        end
+    end
+
+    def update_completed_task
+        @task= Task.find_by(id: params[:id])
+        if @task.nil?
+            head :not_found
+            return 
+        elsif @task.update(
+            name: params[:task][:name],
+            description: params[:task][:description],
+            completed_at: Time.now
+        )
+            redirect_to tasks_path
+            return
+        end
+    end
+
+    def destroy
+        task_id = params[:id]
+        @task = Task.find_by(id: task_id)
+    
+        if @task.nil?
+          head :not_found
+          return
+        end
+    
+        @task.destroy
+    
+        redirect_to tasks_path
+        return
+    end
+
 end

47cd8de69f3b2a8e081c3843bbcdeb6754fb2282
Angela Nguyen <angethuy@gmail.com>
2020-05-04 23:03:05 -0700

    new tests for retrieving edit pages fail

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -28,7 +28,7 @@ describe TasksController do
   # Unskip these tests for Wave 2
   describe "show" do
     it "can get a valid task" do
-      skip
+      
       # Act
       get task_path(task.id)
       
@@ -37,7 +37,7 @@ describe TasksController do
     end
     
     it "will redirect for an invalid task" do
-      skip
+      
       # Act
       get task_path(-1)
       
@@ -48,7 +48,6 @@ describe TasksController do
   
   describe "new" do
     it "can get the new task page" do
-      skip
       
       # Act
       get new_task_path
@@ -60,7 +59,7 @@ describe TasksController do
   
   describe "create" do
     it "can create a new task" do
-      skip
+      
       
       # Arrange
       task_hash = {
@@ -88,13 +87,20 @@ describe TasksController do
   # Unskip and complete these tests for Wave 3
   describe "edit" do
     it "can get the edit page for an existing task" do
-      skip
-      # Your code here
+      # Act
+      get edit_task_path(task.id)
+
+      #Assert 
+      must_respond_with :success
     end
     
     it "will respond with redirect when attempting to edit a nonexistant task" do
-      skip
-      # Your code here
+      # Act
+      get edit_task_path(-1)
+
+      # Assert 
+      must_respond_with :redirect
+      
     end
   end
   
@@ -103,10 +109,12 @@ describe TasksController do
     # Note:  If there was a way to fail to save the changes to a task, that would be a great
     #        thing to test.
     it "can update an existing task" do
+      skip 
       # Your code here
     end
     
     it "will redirect to the root page if given an invalid id" do
+      skip 
       # Your code here
     end
   end

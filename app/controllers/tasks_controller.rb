e25ff4470ffb2001a877a239645b5aa30dc25a94
Maryam Heshmati <mairheshmati@gmail.com>
2020-05-04 17:15:54 -0700

    finished all tests, minor changes in the layout of the Controller Tasks to adjust to the testing requirements

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,7 +10,7 @@ class TasksController < ApplicationController
     @task = Task.find_by(id: task_id)
 
     if @task.nil?
-      head :temporary_redirect
+      head :not_found
       return
     end
   end
@@ -47,16 +47,19 @@ class TasksController < ApplicationController
     task_id = params[:id].to_i
     @task = Task.find_by(id: task_id)
 
-    @task[:name] = params[:task][:name]
-    @task[:description] = params[:task][:description]
-    @task[:completed_at] = params[:task][:completed_at]
-
-    if @task.save
-      redirect_to task_path(@task.id)
-      return
+    if @task.nil?
+      head :not_found
     else
-      render :new, :bad_request
-      return
+      @task[:name] = params[:task][:name]
+      @task[:description] = params[:task][:description]
+      @task[:completed_at] = params[:task][:completed_at]
+      if @task.save
+        redirect_to task_path(@task.id)
+        return
+      else
+        render :new, :bad_request
+        return
+      end
     end
   end
 
@@ -75,16 +78,19 @@ class TasksController < ApplicationController
     task_id = params[:id].to_i
     @task = Task.find_by(id: task_id)
 
-    if @task[:completed_at].empty?
-      @task[:completed_at] = Date.today.to_s
-    end
-
-    if @task.save
-      redirect_to "/tasks"
-      return
+    if @task.nil?
+      head :not_found
     else
-      render :new, :bad_request
-      return
+      if @task[:completed_at].empty?
+        @task[:completed_at] = Date.today.to_s
+      end
+      if @task.save
+        redirect_to "/tasks"
+        return
+      else
+        render :new, :bad_request
+        return
+      end
     end
   end
 end

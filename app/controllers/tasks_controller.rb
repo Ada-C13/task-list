70da22d85d908267f5937253cbf3a78cf4787e4c
Angela Nguyen <angethuy@gmail.com>
2020-05-04 22:38:38 -0700

    added support for creating new tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,21 @@ class TasksController < ApplicationController
 
   def show
     @task = Task.find_by(id: params[:id])
-    return head :not_found if @task.nil?
+    return redirect_to :tasks if @task.nil?
+  end
+
+  def new 
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description], completed_at: params[:task][:completed_at]) #instantiate a new book
+    if @task.save 
+      return redirect_to @task
+    else
+      render :new
+      return
+    end
   end
 
 end

fb341adcb29e2f432562b1e2e360771ea79d4495
Jessica Stone <jstone4815@gmail.com>
2020-05-05 10:04:00 -0700

    update to use strong params

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -16,10 +16,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(
-      name: params[:task][:name],
-      description: params[:task][:description]
-    )
+    task = Task.new(task_params)
 
     if task.save
       redirect_to task_path(task.id)
@@ -41,10 +38,7 @@ class TasksController < ApplicationController
     if @task.nil?
       redirect_to root_path
       return
-    elsif @task.update(
-      name: params[:task][:name],
-      description: params[:task][:description]
-    )
+    elsif @task.update(task_params)
       redirect_to task_path
       return
     else
@@ -81,5 +75,11 @@ class TasksController < ApplicationController
       render :show
       return
     end
+  end
+
+  private
+
+  def task_params
+    return params.require(:task).permit(:name, :description, :completed_at)
   end
 end

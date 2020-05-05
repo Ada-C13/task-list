a2c94e4772f97a29dc1826130c5f6a22dd489999
Shonda860 <norshonda.warren@my.ccsu.edu>
2020-05-05 16:17:34 -0700

    using root and resources

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,16 @@ Rails.application.routes.draw do
 
   #Add Routes that have to with the collection of tasks
   root to: "tasks#index"
-  # resources: :tasks
-  get "tasks", to: "tasks#index", as: "tasks" #all books
-  get "tasks/new", to: "tasks#new", as: "new_task" #gets a form for new
-  post "/tasks", to: "tasks#create"
+  resources :tasks
+  # get "tasks", to: "tasks#index", as: "tasks" #all books
+  # get "tasks/new", to: "tasks#new", as: "new_task" #gets a form for new
+  # post "/tasks", to: "tasks#create"
 
-  #Routes that deal witha a specific Task
-  get "/tasks/:id", to: "tasks#show", as: "task"
-  get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
-  patch "/tasks/:id", to: "tasks#update"
-  delete "/tasks/:id", to: "tasks#destory"
+  # #Routes that deal witha a specific Task
+  # get "/tasks/:id", to: "tasks#show", as: "task"
+  # get "tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  # patch "/tasks/:id", to: "tasks#update"
+  # delete "/tasks/:id", to: "tasks#destory"
   patch "/tasks/:id/mark_complete", to: "tasks#mark_complete", as: "complete"
   patch "/tasks/:id/unmark_complete", to: "tasks#unmark_complete", as: "unmark"
 end

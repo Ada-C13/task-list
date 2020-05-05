7dfe3a00ea6307968fd25d91de4e9535faf2e856
kate myer <kate.d.mangubat@gmail.com>
2020-05-04 17:54:42 -0700

    added root with brittany

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # verb 'path', to: 'controller#action'
   get '/tasks', to: 'tasks#index'
+  # verb 'root', to: 'controller#action'
+  root 'tasks#index'
 end

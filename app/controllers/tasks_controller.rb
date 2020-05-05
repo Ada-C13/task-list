0101ee3b2305ff73222c74aa9de02c817b71409b
Antonia <antonianomamiukor@gmail.com>
2020-05-05 10:14:52 -0700

    changed some formatting

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,43 +27,43 @@ class TasksController < ApplicationController
       else
         render :new, :bad_request
         return
+      end
     end
-end
-
-def edit
-  @task = Task.find_by(id: params[:id])
-  if @task.nil?
-    redirect_to root_path
-    return
+    
+    def edit
+      @task = Task.find_by(id: params[:id])
+      if @task.nil?
+        redirect_to root_path
+        return
+      end
+    end
+    
+    def update
+      @task = Task.find_by(id: params[:id])
+      if @task.nil?
+        redirect_to root_path
+        return
+      elsif @task.update(
+        name: params[:task][:name],
+        description: params[:task][:description])
+        redirect_to task_path(@task.id)
+        return
+      else
+        render :edit
+        return
+      end
+    end
+    
+    def destroy
+      id = params[:id]
+      @task = Task.find_by(id: id)
+      if @task.nil?
+        redirect_to root_path
+        return
+      else
+        @task.destroy
+        redirect_to root_path
+      end
+    end
+    
   end
-end
-
-def update
-  @task = Task.find_by(id: params[:id])
-  if @task.nil?
-    redirect_to root_path
-    return
-  elsif @task.update(
-    name: params[:task][:name],
-    description: params[:task][:description])
-    redirect_to task_path(@task.id)
-    return
-  else
-    render :edit
-    return
-  end
-end
-
-def destroy
-  id = params[:id]
-  @task = Task.find_by(id: id)
-  if @task.nil?
-    redirect_to root_path
-    return
-  else
-    @task.destroy
-    redirect_to root_path
-end
-end
-
-end

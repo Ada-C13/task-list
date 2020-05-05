0b72660cda0462c3504e8cb5e2ad0ade3dd753cf
Katie <katie.vandervoort@gmail.com>
2020-05-05 01:28:05 -0700

    created the create function to make a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,6 +21,16 @@ class TasksController < ApplicationController
     @task = Task.new
   end
 
-  
+  def create
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description]) # instantiate a new task
+    
+    if @task.save # save returns true if the database insert succeeds
+      redirect_to tasks_path # go to the index so we can see the new task in the list
+      return
+    else # save failed :(
+      render :new # show the new task form view again
+      return
+    end
+  end
 
 end

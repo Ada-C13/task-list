2d1e2d3de3432129e45fca342089757152ea5403
Alicia Combs <alicia.combs.92@gmail.com>
2020-05-04 17:46:08 -0700

    Added show.html.erb, show controller action, and all the routes needed for this project

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,4 +4,14 @@ class TasksController < ApplicationController
     # @tasks_array = ["Run Laundry", "Make Tea", "Finish a Sewing Project", "Draft a Sewing Pattern", "Weed the Yard"]
     @tasks = Task.all
   end
+
+  def show
+    task_id = params[:id].to_i
+    @task = Task.find_by(id: task_id)
+
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
 end

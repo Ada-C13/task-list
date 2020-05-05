0db95159317395c8f3130364f6a66929fbbed256
peachmakkoli <sara.lee.higgins@gmail.com>
2020-05-04 21:58:49 -0700

    adds tasks#update action and patch route

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -17,7 +17,11 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(name: params[:task][:name], description: params[:task][:description])
+    @task = Task.new(
+      name: params[:task][:name], 
+      description: params[:task][:description]
+    )
+
     @task.save ? (redirect_to task_path(@task)) : (render :new, alert: "Error: Task not saved")
     return 
   end
@@ -29,5 +33,20 @@ class TasksController < ApplicationController
       redirect_to root_path, alert: "#{error}"
       return
     end
+  end
+
+  def update
+    begin
+      @task = Task.find(params[:id]) 
+    rescue => error
+      redirect_to root_path, alert: "#{error}"
+      return
+    end
+
+    @task.update(
+      name: params[:task][:name], 
+      description: params[:task][:description]
+    ) ? (redirect_to task_path(@task)) : (render :edit, alert: "Error: Task not updated")
+    return
   end
 end

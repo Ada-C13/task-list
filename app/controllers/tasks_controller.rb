8f93f72fa788e0640f894d9789c4bb59a9666448
Angela Nguyen <angethuy@gmail.com>
2020-05-04 21:09:44 -0700

    added support to show each task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,10 @@ class TasksController < ApplicationController
   def index
     @tasks = Task.all
   end
+
+  def show
+    @task = Task.find_by(id: params[:id])
+    return head :not_found if @task.nil?
+  end
+
 end

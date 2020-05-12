7a8311596de269bd43cda92996dc56ac014e5d21
Hannah Tekié <hannahtekie@Hannahs-MacBook-Pro.local>
2020-05-12 11:52:32 -0700

    did till wave two for the 3rd time, test is not working, Jared is helping

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "tasks#index"
+  get '/tasks', to:'tasks#index', as: 'tasks'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
+
+
+  # routes that deal with a specific task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch '/tasks/:id', to: 'task#update'
+  delete '/tasks/:id', to: 'task#destroy'
+  # This is a custom route, and in this case, the mark_completed! 
+  patch '/tasks/:id/mark_complete', to: 'tasks#mark_complete', as: 'mark_completed' 
 end
+
+
+
+
+ 

c7876b64636c7d8d97646cd6e374878090d6931a
Yaz <yasmineoshaugh@gmail.com>
2020-05-16 14:44:33 -0700

    created form for new task in new.html.erb, corresponding task#actions create and new, and coressponding route. Link to add new task is now at the bottom of index page.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,9 @@
 class TasksController < ApplicationController
+
   def index
     @tasks = Task.all
   end
+
   def show
     task_id = params[:id]
     @task = Task.find_by(id: task_id)
@@ -10,4 +12,20 @@ class TasksController < ApplicationController
       return
     end
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create 
+    @task = Task.new(name: params[:task][:name], description: params[:task][:description])
+    if @task.save
+      redirect_to tasks_path
+      return
+    else
+      render :new
+      return
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # collection of tasks
   get '/tasks', to: 'tasks#index', as: 'tasks'
+  get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  post '/tasks', to: 'tasks#create'
 
   # individual tasks
   get '/tasks/:id', to: 'tasks#show', as: 'task'

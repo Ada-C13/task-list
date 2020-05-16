722586b4332426124ce07b1db67e4df0ad258a22
Lola Llanes <mslourdesllanes@gmail.com>
2020-05-15 18:30:06 -0700

    updated resftul routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,17 +4,11 @@ Rails.application.routes.draw do
   root to: "tasks#index"
 
   get '/tasks', to: 'task#index', as: 'tasks'
-
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
-
   get '/tasks/:id', to: 'task#show', as: 'task'
-
   post '/tasks', to: 'task#create'
-
   get '/tasks/edit', to: 'tasks#edit', as: 'edit_task'
-
   patch '/tasks/:id', to: 'tasks#update'
-
   delete '/tasks/id:', to: 'task#destroy'
 
 end

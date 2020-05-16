33ac291f17cea0cd039bed6649df00ce82a01ce4
Lola Llanes <mslourdesllanes@gmail.com>
2020-05-15 20:09:01 -0700

    fixed task to tasks (per yieni)

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,14 @@ Rails.application.routes.draw do
 
   root to: "tasks#index"
 
-  get '/tasks', to: 'task#index', as: 'tasks'
+  get '/tasks', to: 'tasks#index', as: 'tasks'
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  get '/tasks/:id', to: 'task#show', as: 'task'
-  post '/tasks', to: 'task#create'
-  get '/tasks/edit', to: 'tasks#edit', as: 'edit_task'
+  post '/tasks', to: 'tasks#create'
+
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
   patch '/tasks/:id', to: 'tasks#update'
-  delete '/tasks/id:', to: 'task#destroy'
+  delete '/tasks/:id', to: 'tasks#destroy'
+  patch '/tasks/:id/mark_complete', to: 'tasks#mark_complete', as: 'mark_complete'
 
 end

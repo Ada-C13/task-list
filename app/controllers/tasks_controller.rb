463d737202451fb6e7b595d61c5ddbcb978a4e28
Yaz <yasmineoshaugh@gmail.com>
2020-05-16 17:30:44 -0700

    created edit form and mark complete option

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,8 +5,7 @@ class TasksController < ApplicationController
   end
 
   def show
-    task_id = params[:id]
-    @task = Task.find_by(id: task_id)
+    @task = Task.find_by(id: params[:id])
     if @task.nil?
       head :not_found
       return
@@ -24,6 +23,42 @@ class TasksController < ApplicationController
       return
     else
       render :new
+      return
+    end
+  end
+
+  def edit 
+    @task = Task.find_by(id: params[:id])
+    if @task.nil?
+      head :not_found
+      return
+    end
+  end
+
+  def update
+    @task = Task.find_by(id: params[:id])
+    if @task.nil?
+      head :not_found
+      return
+    elsif @task.update(name: params[:task][:name], description: params[:task][:description])
+      redirect_to task_path(@task) 
+      return
+    else
+      render :edit
+      return
+    end
+  end
+
+  def mark_complete
+    @task = Task.find_by(id: params[:id])
+    if @task.nil?
+      head :not_found
+      return
+    else
+      @task.completed = true
+      @task.completed_at = Time.now.to_s
+      @task.save
+      redirect_to tasks_path
       return
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_16_032507) do
+ActiveRecord::Schema.define(version: 2020_05_16_232451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_05_16_032507) do
     t.string "completed_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "completed", default: false
   end
 
 end
